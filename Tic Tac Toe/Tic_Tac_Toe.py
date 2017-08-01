e161a91cd49e71885227ed6a0e2f56a1ba8687c9
Shawn Staggs <shawn.staggs@gmail.com>
2017-08-01 13:59:06 -0500

    Initial psuedo code

--- Tic Tac Toe/Tic_Tac_Toe.py
+++ Tic Tac Toe/Tic_Tac_Toe.py
@@ -1,1 +1,10 @@
+# Create Tic Tac Toe for the python bootcamp
+# Following is the psuedo code for what it will do.
 
+# Start the game.
+# Print greeting.
+# Get player count/names.
+# if 1 player
+#   use AI logic
+# else
+#   use two player logic
