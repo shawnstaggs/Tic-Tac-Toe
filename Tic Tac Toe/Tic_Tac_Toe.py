ff0b51a9b76e08312ec4329126f445094d0b198f
Shawn Staggs <shawn.staggs@gmail.com>
2017-08-03 17:29:23 -0500

    peg out function names

--- Tic Tac Toe/Tic_Tac_Toe.py
+++ Tic Tac Toe/Tic_Tac_Toe.py
@@ -1,10 +1,57 @@
 # Create Tic Tac Toe for the python bootcamp
-# Following is the psuedo code for what it will do.
+from __future__ import print_function
+from IPython.display import clear_output
+def display_board(board):
+    
+    pass
 
-# Start the game.
-# Print greeting.
-# Get player count/names.
-# if 1 player
-#   use AI logic
-# else
-#   use two player logic
+def player_input():
+    pass
+
+def place_marker(board, marker, position):
+    pass
+
+def win_check(board, mark):
+    pass
+
+import random
+def choose_first():
+    print("Player %s goes first.", random.choice([1,2]))
+
+def space_check(board, position):
+
+    pass
+
+def full_board_check(board):
+
+    pass
+
+def player_choice(board):
+
+    pass
+
+def replay():
+    again = input("Would you like to play again?(Y/N)")
+    if again.isalpha():
+        if again[0] == 'y':
+            return True
+        else:
+            return False
+    else:
+        replay()
+
+print('Welcome to Tic Tax Toe!')
+
+while True:
+    choose_first()
+
+#    set up the game
+
+#    while game on 
+#        player 1 turn
+
+#        player 2 turn
+
+
+    if not replay():
+        break
